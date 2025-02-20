3c22f55088586052001212c3d63d476238242c7e
Miriam <if23b205@technikum-wien.at>
2025-02-20 17:24:51 +0100

    added Option Presentation

--- src/main/java/dev/icefish/tourplanner/HelloController.java
+++ src/main/java/dev/icefish/tourplanner/HelloController.java
@@ -1,6 +1,7 @@
 package dev.icefish.tourplanner;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 
 public class HelloController {
@@ -8,8 +9,15 @@ public class HelloController {
     private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Lol");
-    }
+    private ComboBox<String> myComboBox;
 
+    @FXML
+    protected void onHelloButtonClick() {
+        String selectedValue = myComboBox.getValue(); // Wert der ComboBox holen
+        if (selectedValue != null) {
+            welcomeText.setText(selectedValue); // Den Wert ausgeben
+        } else {
+            welcomeText.setText("Bitte eine Option auswählen!");
+        }
+    }
 }
