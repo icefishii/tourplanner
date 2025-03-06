fd79f6a6d6be2799e815bf0240b1d888a0c62ae2
Miriam <if23b205@technikum-wien.at>
2025-03-06 13:51:35 +0100

    tour class

--- src/main/java/dev/icefish/tourplanner/models/Tour.java
+++ src/main/java/dev/icefish/tourplanner/models/Tour.java
@@ -1,9 +1,115 @@
 package dev.icefish.tourplanner.models;
 
 
+import java.util.UUID;
+
 public class Tour {
+    private UUID id;
+
+    private String name;
+    private String description;
+    private String fromLocation;
+    private String toLocation;
+    private String transportType;
+    private double distance;
+    private double estimatedTime;
+    private String routeImagePath;
 
 
+    //wenn noch nicht die time und distance mit API berechnet wird
+    public Tour(String name, String description, String fromLocation, String toLocation, String transportType) {
+        this.name = name;
+        this.description = description;
+        this.toLocation = toLocation;
+        this.fromLocation = fromLocation;
+        this.transportType = transportType;
+    }
+
+    public Tour(String name, String description, String fromLocation, String toLocation, String transportType, double distance, double estimatedTime, String routeImagePath) {
+        this.id = UUID.randomUUID();
+        this.name = name;
+        this.description = description;
+        this.fromLocation = fromLocation;
+        this.toLocation = toLocation;
+        this.transportType = transportType;
+        this.distance = distance;
+        this.estimatedTime = estimatedTime;
+        this.routeImagePath = routeImagePath;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getFromLocation() {
+        return fromLocation;
+    }
+
+    public void setFromLocation(String fromLocation) {
+        this.fromLocation = fromLocation;
+    }
+
+    public String getToLocation() {
+        return toLocation;
+    }
+
+    public void setToLocation(String toLocation) {
+        this.toLocation = toLocation;
+    }
+
+    public String getTransportType() {
+        return transportType;
+    }
+
+    public void setTransportType(String transportType) {
+        this.transportType = transportType;
+    }
+
+    public double getDistance() {
+        return distance;
+    }
+
+    public void setDistance(double distance) {
+        this.distance = distance;
+    }
+
+    public double getEstimatedTime() {
+        return estimatedTime;
+    }
+
+    public void setEstimatedTime(double estimatedTime) {
+        this.estimatedTime = estimatedTime;
+    }
+
+    public String getRouteImagePath() {
+        return routeImagePath;
+    }
+
+    public void setRouteImagePath(String routeImagePath) {
+        this.routeImagePath = routeImagePath;
+    }
+
+    //Vllt Tourlogs eine Liste aus Tours??
 
 }
 
