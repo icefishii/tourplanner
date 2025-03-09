bebf070f1c2a89c51701b46b76ff69e6de8e80b7
Benjamin Lampart <sera@icefish.dev>
2025-03-09 19:16:51 +0100

    Fixed wrong path to fxml

--- client/src/main/java/dev/icefish/tourplanner/client/Client.java
+++ client/src/main/java/dev/icefish/tourplanner/client/Client.java
@@ -10,7 +10,7 @@ import java.io.IOException;
 public class Client extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(Client.class.getResource("/dev/icefish/tourplanner/client/TourPlannerWindow.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(Client.class.getResource("/TourPlannerWindow.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 620, 440);
         stage.setTitle("Tour Planner System");
         stage.setScene(scene);
