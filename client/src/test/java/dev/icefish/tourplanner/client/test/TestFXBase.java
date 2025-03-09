aedf468e52ab40351c595ddb0510928b5ce84cf6
Benjamin Lampart <sera@icefish.dev>
2025-03-09 18:16:11 +0100

    Add unit tests for tour creation and editing functionality

---
+++ client/src/test/java/dev/icefish/tourplanner/client/test/TestFXBase.java
@@ -0,0 +1,42 @@
+package dev.icefish.tourplanner.client.test;
+
+import java.util.concurrent.TimeoutException;
+import javafx.scene.Node;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.MouseButton;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.testfx.api.FxToolkit;
+import org.testfx.framework.junit5.ApplicationTest;
+
+public abstract class TestFXBase extends ApplicationTest {
+
+    @BeforeAll
+    public static void setupHeadlessMode() {
+        System.setProperty("testfx.robot", "glass");
+        System.setProperty("testfx.headless", "true");
+        System.setProperty("prism.order", "sw");
+        System.setProperty("prism.text", "t2k");
+        System.setProperty("java.awt.headless", "true");
+        System.setProperty("glass.platform", "Monocle");
+        System.setProperty("monocle.platform", "Headless");
+    }
+
+    @BeforeEach
+    public void setUp() throws Exception {
+        FxToolkit.registerPrimaryStage();
+    }
+
+    @AfterEach
+    public void tearDown() throws TimeoutException {
+        FxToolkit.hideStage();
+        release(new KeyCode[] {});
+        release(new MouseButton[] {});
+    }
+
+    // Helper methods
+    protected <T extends Node> T find(String query) {
+        return lookup(query).query();
+    }
+}
