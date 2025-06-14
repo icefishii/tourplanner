2c91ac59829a8c7d3d98063c794126546f509420
Miriam <if23b205@technikum-wien.at>
2025-06-14 15:50:52 +0200

    Path changed

--- client/src/main/java/dev/icefish/tourplanner/client/viewmodel/MapViewModel.java
+++ client/src/main/java/dev/icefish/tourplanner/client/viewmodel/MapViewModel.java
@@ -1,22 +1,24 @@
 package dev.icefish.tourplanner.client.viewmodel;
-
+import dev.icefish.tourplanner.client.utils.ConfigLoader;
 import javafx.beans.property.ObjectProperty;
 import javafx.beans.property.SimpleObjectProperty;
 import javafx.scene.image.Image;
 
 import java.io.File;
 import java.util.UUID;
+import java.nio.file.Paths;
 
 public class MapViewModel {
 
     private final ObjectProperty<Image> currentMapImage = new SimpleObjectProperty<>();
 
-    private static final String MAPS_FOLDER_PATH = "C:/Users/miria/IdeaProjects/tourplanner/client/maps/"; //ToDo Pfad
+    private final String imageBasePath;
     private static final String DEFAULT_IMAGE_PATH = "/images/maps.jpeg"; // Dein Fallback-Bild aus dem Ressourcenordner
 
     public MapViewModel() {
         // Setze Standardbild beim Start
-        currentMapImage.set(new Image(getClass().getResource(DEFAULT_IMAGE_PATH).toExternalForm()));
+        this.imageBasePath = ConfigLoader.get("image.basePath");
+        setDefaultImage();
     }
 
     public ObjectProperty<Image> currentMapImageProperty() {
@@ -32,8 +34,8 @@ public class MapViewModel {
             setDefaultImage();
             return;
         }
-        String filePath = MAPS_FOLDER_PATH + tourId.toString() + ".png";
-        File imageFile = new File(filePath);
+        File imageFile = Paths.get(imageBasePath, tourId.toString() + ".png").toFile();
+
         if (imageFile.exists()) {
             currentMapImage.set(new Image(imageFile.toURI().toString()));
         } else {
